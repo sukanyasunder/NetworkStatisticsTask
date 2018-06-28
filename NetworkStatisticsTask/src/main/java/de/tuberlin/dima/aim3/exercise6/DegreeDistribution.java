203302b4dec15165448b1fa1707dfe909942823f
Pandu Wicaksono <pandu.wicaksono91@gmail.com>
2018-06-28 15:39:55 +0200

    update Copyright

--- NetworkStatisticsTask/src/main/java/de/tuberlin/dima/aim3/exercise6/DegreeDistribution.java
+++ NetworkStatisticsTask/src/main/java/de/tuberlin/dima/aim3/exercise6/DegreeDistribution.java
@@ -1,6 +1,6 @@
 /**
  * AIM3 - Scalable Data Mining -  course work
- * Copyright (C) 2014  Pandu Wicaksono
+ * Copyright (C) 2018  Pandu Wicaksono
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
